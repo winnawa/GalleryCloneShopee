91049580be5602a47358d2f060a557e1ffa0c41c
Nam Khoa <khoa.pham28042002@hcmut.edu.vn>
2022-07-08 18:39:52 +0700

    style component as TS

--- frontEnd/src/component/Body/SlideStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Container=styled.div`
-    backgroundSize : contain;
-    backgroundRepeat: no-repeat;
-    width: 500px;
-    height: 200px;
-`;
